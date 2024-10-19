d22d47cc212b649d850b967491e645ee65c7ce61
DigiCris <cmarchesetdiii@gmail.com>
2024-10-19 19:10:50 -0300

    NFT added

---
+++ smart-contracts/contracts/simpleNFT.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact cmarchese@comunyt.com
+contract BudyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    uint256 public _nextTokenId;
+    uint256 public immutable price = 3 ether;
+
+    constructor(address initialOwner)
+        ERC721("BudyNFT", "BNFT")
+        Ownable(msg.sender)
+    {}
+
+    function withdraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function safeMint(address to, string memory uri) public payable  {
+        require(msg.value==price,"Nos the money to send");
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Enumerable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
